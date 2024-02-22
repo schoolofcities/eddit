83a72b759f548c4040fc2777673bb3718380cb21
Jeff Allen <jeffallen89@gmail.com>
2024-02-22 17:31:05 -0500

    initial svelte setup

---
+++ src/routes/high-point/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+
+	import TopSofC from "../../lib/TopSofC.svelte";
+	import "../../assets/global-styles.css";
+	
+</script>
+
+
+
+
+
+
+<TopSofC/>
+
+<main>
+
+
+
+</main>
+
+
+
+
+
+<style>
+
+
+	
+</style>
+
