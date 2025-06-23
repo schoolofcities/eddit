408555e9985218bd5054b7e3d7ff7c4d48cecc08
Jeff Allen <jeffallen89@gmail.com>
2025-06-23 13:15:48 -0400

    cook county maps

--- src/routes/cook-county-il/+page.svelte
+++ src/routes/cook-county-il/+page.svelte
@@ -6,7 +6,7 @@
 	import Footer from "$lib/Footer.svelte";
 	import SingleGraphic from "$lib/SingleGraphic.svelte";
 	import SingleGraphicLazy from "$lib/SingleGraphicLazy.svelte";
-	import SmallMultiples from "$lib/SmallMultiples.svelte";
+	import SmallMultiplesLazy from "$lib/SmallMultiplesLazy.svelte";
 	import SingleImage from "$lib/SingleImage.svelte";
 
 	import titleImage from "./assets/cook-county-title.jpg";
@@ -43,6 +43,17 @@
 		svg360={"./cook-county-il/web-svg/chicago-contextmap-360.svg"}
 	/>
 
+	<div class="text">
+
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+		<p>
+			Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+
+	</div>
+
 	<SingleGraphicLazy
 		svg720={"./cook-county-il/web-svg/cook-county-contextmap-720.svg"}
 		svg360={"./cook-county-il/web-svg/cook-county-contextmap-360.svg"}
@@ -59,9 +70,66 @@
 
 	</div>
 
+	<SmallMultiplesLazy
+		svgPaths={["./cook-county-il/web-svg/cook-county_income-map-360.svg", "./cook-county-il/web-svg/cook-county_minorities-map-360.svg"]}
+	/>
 
+	<div class="text">
 
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+		<p>
+			Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
 
+	</div>
+
+	<SmallMultiplesLazy
+		svgPaths={["./cook-county-il/web-svg/cook-county_NO2-map-360.svg", "./cook-county-il/web-svg/cook-county_PM25-pollution-map-360.svg", "./cook-county-il/web-svg/cook-county_ejc-map-360.svg"]}
+	/>
+
+	<div class="text">
+
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+		<p>
+			Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+
+	</div>
+
+	<SingleGraphicLazy
+		svg720={"./cook-county-il/web-svg/cook-county_gasvelectric-chart-720.svg"}
+		svg360={"./cook-county-il/web-svg/cook-county_gasvelectric-chart-360.svg"}
+	/>
+
+	<div class="text">
+
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+		<p>
+			Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+
+	</div>
+
+	<SmallMultiplesLazy
+		svgPaths={["./cook-county-il/web-svg/cook-county_asthma-map-360.svg", "./cook-county-il/web-svg/cook-county_dr-visits-map-360.svg"]}
+	/>
+
+	<div class="text">
+
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+		<p>
+			Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+		</p>
+
+	</div>
 
 
 </main>
