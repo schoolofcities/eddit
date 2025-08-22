4f119513075b824f5e7e8658009e43f2fcd8b878
isabeauxgraham <isabeaux.graham@mail.utoronto.ca>
2025-08-22 13:01:19 -0700

    eau claire text + graphics

--- src/lib/svg-conversion.py
+++ src/lib/svg-conversion.py
@@ -414,7 +414,7 @@ from pathlib import Path
 
 
 def main():
-	city = "wood-buffalo-ab"
+	city = "eau-claire-wi"
 	input_dir = "../routes/" + city + "/assets"  
 	output_dir = "../../static/"  + city + "/web-svg"
 	# input_dir = "../assets/landing/map"
